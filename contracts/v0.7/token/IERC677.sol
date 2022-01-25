26af6d99a797b8e83f5e67e960f6bab3a5c97ef4
vegita <vegita@gmail.com>
2022-01-25 10:07:35 +0700

    change solidity version

--- contracts/v0.7/token/IERC677.sol
+++ contracts/v0.7/token/IERC677.sol
@@ -1,21 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >0.6.0 <0.8.0;
+pragma solidity >0.8.0;
 
-import "../../../vendor/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import '../../../vendor/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
 
 interface IERC677 is IERC20 {
-  function transferAndCall(
-    address to,
-    uint value,
-    bytes memory data
-  )
-    external
-    returns (bool success);
+    function transferAndCall(
+        address to,
+        uint256 value,
+        bytes memory data
+    ) external returns (bool success);
 
-  event Transfer(
-    address indexed from,
-    address indexed to,
-    uint value,
-    bytes data
-  );
+    event Transfer(address indexed from, address indexed to, uint256 value, bytes data);
 }
