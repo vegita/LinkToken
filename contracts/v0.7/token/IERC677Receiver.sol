612412ae84df236d5ab7c27fac72b88bcd8047d4
vegita <vegita@gmail.com>
2022-01-25 10:51:30 +0700

    solidity v0.8 update

--- contracts/v0.7/token/IERC677Receiver.sol
+++ contracts/v0.7/token/IERC677Receiver.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >0.6.0 <0.8.0;
+pragma solidity >0.8.0;
 
 interface IERC677Receiver {
-  function onTokenTransfer(
-    address sender,
-    uint value,
-    bytes memory data
-  )
-    external;
+    function onTokenTransfer(
+        address sender,
+        uint256 value,
+        bytes memory data
+    ) external;
 }
